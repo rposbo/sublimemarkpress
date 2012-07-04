25936f7d38643dd4d5649fbd254be129309d9999
robin osborne <robin osborne@gmail.com>
2012-07-04 22:18:05 +0100

    updating comments and readme

--- sublimemarkpress.py
+++ sublimemarkpress.py
@@ -13,8 +13,6 @@ class PublishCommand(sublime_plugin.TextCommand):
 			    "password": <password>
 			}
 
-		# key mapping -> doesn't pass the view, so not sure how to do this correctly yet.
-
 		# tags
 		blog tags are optional at the top of the file in the structure:
 		<!-- 
@@ -29,6 +27,11 @@ class PublishCommand(sublime_plugin.TextCommand):
 
 		# markdown
 		If the file "markdown2.py" from the awesome repo https://github.com/trentm/python-markdown2/tree/master/lib exists, markdown is enabled
+
+		# usage
+		Currently, you need to copy this file into the sublimetext packages/user directory. Then on the file you wish to post press ctrl+' and type "view.run_command('publish')"
+		
+		# key mapping -> doesn't pass the view, so not sure how to do this correctly yet.
 	"""
 	def run(self, edit):
 		# load settings
